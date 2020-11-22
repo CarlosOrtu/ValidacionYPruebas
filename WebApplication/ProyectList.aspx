e33cc91594d8590bf072a3f3ef979369a46f2d59
Guillermo Saldaña Suárez <gss1001@alu.ubu.es>
2020-11-22 12:06:11 +0100

    CAmbios en la web

---
+++ WebApplication/ProyectList.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProyectList.aspx.cs" Inherits="WebApplication.ProyectList" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox ID="ListBox1" runat="server" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Width="295px"></asp:ListBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication/ProyectList.aspx.cs
@@ -0,0 +1,37 @@
+﻿using ClassLib;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class ProyectList : System.Web.UI.Page
+    {
+
+        DBPruebas dataBase;
+        Usuario user;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Variable de aplicación
+            dataBase = (DBPruebas)Application["Base de Datos"];
+            if (dataBase == null)
+            {
+                dataBase = new DBPruebas();
+                Application["Base de Datos"] = dataBase;
+            }
+
+            //Variable de sesión. 
+            user = (Usuario)Session["Usuario"];
+
+            if(user.Lista_proyectos != null)
+            {
+                ListBox1.Text = user.Lista_proyectos.ToString();
+            }
+        }
+    }
+}
